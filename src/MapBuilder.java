ff79f25cf1478c505c4ab2ff8c0f3500e7a7a90b
Eric Callaway <ericcallaway5@gmail.com>
2022-07-09 06:31:41 -0500

    [WIP] Added all the paths from root node to destination. I still need to be able to return the shortest path.

--- src/MapBuilder.java
+++ src/MapBuilder.java
@@ -2,10 +2,10 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.LinkedList;
-import java.util.Map;
 import java.util.Queue;
 import java.util.Scanner;
-
+import java.util.Stack;
+import java.util.List;
 
 public class MapBuilder{
 
@@ -38,14 +38,93 @@ public class MapBuilder{
 
     }
 
+    		
 
+    private ArrayList<Integer>[] adjList;
+    
 
+    @SuppressWarnings("unchecked")
+    private void initAdjList(){
+        adjList = new ArrayList[20];
 
-    void addEdge(int v,int w)
-    {
-        adj[v].add(w);                          //adding an edge to the adjacency list (edges are bidirectional in this example)
+        for (int i = 0; i < 20; i++){
+            adjList[i] = new ArrayList<>();
+        }
     }
 
+    // void shortest_path()
+    
+
+
+    void addEdge(int u,int v)
+    {
+        adj[u].add(v);
+        adjList[u].add(v);                          //adding an edge to the adjacency list (edges are bidirectional in this example)
+    }
+
+    public List<List<Integer>> shortest_path = new ArrayList<>();
+    public void printAllPaths(int s, int d ){
+        boolean[] isVisited = new boolean[20];
+        ArrayList<Integer> pathList = new ArrayList<>();
+
+        pathList.add(s);
+        // shortest_path.add(pathList);
+        // for (int i = 0; i < shortest_path.size(); i++){
+        //         System.out.println("Printing out shortest path variable: " + shortest_path);
+        //         System.out.println("Printing out shortest path size: " + shortest_path.size());
+        //     }
+
+        printAllPathsUtil(s, d, isVisited, pathList);
+    }
+
+    // A recursive function to print
+    // all paths from 'u' to 'd'.
+    // isVisited[] keeps track of
+    // vertices in current path.
+    // localPathList<> stores actual
+    // vertices in the current path
+
+    
+    private void printAllPathsUtil(Integer u, Integer d,
+                                   boolean[] isVisited,
+                                   ArrayList<Integer> localPathList)
+    {
+        
+ 
+        if (u.equals(d)) {
+            System.out.println(localPathList);
+            // if match found then no need to traverse more till depth
+            // shortest_path.add(localPathList);
+            // for (int i = 0; i < shortest_path.size(); i++){
+            //     System.out.println("Printing out shortest path variable: " + shortest_path);
+            //     System.out.println("Printing out shortest path size: " + shortest_path.size());
+            // }
+            return;
+        }
+ 
+        // Mark the current node
+        isVisited[u] = true;
+ 
+        // Recur for all the vertices
+        // adjacent to current vertex
+        for (Integer i : adjList[u]) {
+            if (!isVisited[i]) {
+                // store current node
+                // in path[]
+                localPathList.add(i);
+                printAllPathsUtil(i, d, isVisited, localPathList);
+ 
+                // remove current node
+                // in path[]
+                localPathList.remove(i);
+            }
+        }
+ 
+        // Mark the current node
+        isVisited[u] = false;
+    }
+
+    // This method takes in an integer n, which is the ID of the root node, the list of cities, and the destination node.
     void breadthFirst(int n, ArrayList<node> nodeList, node destination_node)
     {
         node dest_node = destination_node;
@@ -111,6 +190,71 @@ public class MapBuilder{
                 break;
             }
 
+        }
+    }
+
+    void depthFirst(int n, ArrayList<node> nodeList, node destination_node){
+        boolean nodes[] = new boolean[20];
+
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(n); // Push root node onto stack
+        int current = 0;
+
+        while(!stack.empty()){
+            n = stack.peek();
+            stack.pop();
+
+            node curr_n = find_node(n, nodeList);
+
+            if(nodes[n] == false){
+                // System.out.println("This node has not been visited: ");
+                System.out.println("We are currently at city: " + curr_n.name + ", ID: " + n);
+                nodes[n] = true;
+            }
+
+
+            // System.out.println("Size of the path to node " + curr_n.name  + " is " + curr_n.path_to_node.size());
+
+            // if (!curr_n.path_to_node.contains(curr_n.name)){
+            //     System.out.println(curr_n.name + "'s path does not have the duplicate city " + curr_n.name);
+            //     curr_n.path_to_node.add(curr_n.name);
+            //     for (int k = 0; k < curr_n.path_to_node.size(); k++){
+            //         if (!curr_n.path_to_node.get(k).contains(curr_n.name)){
+            //             curr_n.path_to_node.add(curr_n.name);
+            //             System.out.println("Adding node: " + curr_n.path_to_node.get(k));
+            //             break;
+            //         }
+            //     }
+            // }
+
+            
+            
+
+
+            System.out.println("The path to city: " + curr_n.name + " is " + curr_n.path_to_node);
+
+            for (int i = 0; i < adj[n].size(); i++){
+                
+                current = adj[n].get(i);
+                if(!nodes[current]){
+                    node temp = find_node(current, nodeList);
+                    node parent = find_node(n, nodeList);
+
+                    // for(int j = 0; j < parent.path_to_node.size(); j++){
+                    //     temp.path_to_node.add(parent.path_to_node.get(j));
+                    // }
+
+                    if(!temp.path_to_node.contains(n)){
+                        temp.path_to_node.add(nodeList.get(n).name);
+                        temp.path_to_node.add(temp.name);
+                    }
+                    System.out.println("Path to node: " + temp.name + " - " + temp.path_to_node.toString());
+
+                    stack.push(current);
+                }
+            }
+            System.out.println();
         }
     }
     public static void main(String[] args){
@@ -181,6 +325,81 @@ public class MapBuilder{
 
         node dest_node = c12;
         MapBuilder mb = new MapBuilder(nodeList.size());
+        mb.initAdjList();
+
+        mb.addEdge(0, 1);
+        mb.addEdge(0, 7);
+
+        mb.addEdge(1, 0);
+        mb.addEdge(1, 2);
+
+        mb.addEdge(2, 1);
+        mb.addEdge(2, 7);
+        mb.addEdge(2, 3);
+
+        mb.addEdge(3, 2);
+        mb.addEdge(3, 4);
+
+        mb.addEdge(4, 3);
+        mb.addEdge(4, 5);
+
+        mb.addEdge(5, 4);
+        mb.addEdge(5, 6);
+
+        mb.addEdge(6, 5);
+        mb.addEdge(6, 9);
+
+        mb.addEdge(7, 0);
+        mb.addEdge(7, 2);
+        mb.addEdge(7, 8);
+        mb.addEdge(7, 11);
+
+        mb.addEdge(8, 7);
+        mb.addEdge(8, 9);
+        mb.addEdge(8, 10);
+
+        mb.addEdge(9, 6);
+        mb.addEdge(9, 8);
+        mb.addEdge(9, 10);
+
+        mb.addEdge(10, 8);
+        mb.addEdge(10, 9);
+        mb.addEdge(10, 12);
+
+        mb.addEdge(11, 7);
+        mb.addEdge(11, 12);
+
+        mb.addEdge(12, 11);
+        mb.addEdge(12, 10);
+        mb.addEdge(12, 13);
+        mb.addEdge(12, 14);
+
+        mb.addEdge(13, 12);
+
+        mb.addEdge(14, 12);
+        mb.addEdge(14, 15);
+        mb.addEdge(14, 17);
+
+        mb.addEdge(15, 14);
+        mb.addEdge(15, 16);
+
+        mb.addEdge(16, 15);
+
+        mb.addEdge(17, 18);
+        mb.addEdge(17, 14);
+
+        mb.addEdge(18, 17);
+        mb.addEdge(18, 19);
+
+        mb.addEdge(19, 18);
+
+        
+        int source = 7;
+        int destination = 12;
+        mb.printAllPaths(source, destination);
+        
+            
+        
 
         Collections.sort(nodeList, new Comparator<node>() {
             public int compare(node n1, node n2){
@@ -219,8 +438,13 @@ public class MapBuilder{
 
 
 
-        System.out.println("Printing out Breadth First Search Traversal");
-        mb.breadthFirst(root_node.ID, nodeList, dest_node);
+        // System.out.println("Printing out Breadth First Search Traversal");
+        // mb.breadthFirst(root_node.ID, nodeList, dest_node);
+        System.out.println("Printing out Depth First Search Traversal.");
+        System.out.println("Following are all different paths from Sibiu to Bucharest.");
+        
+        
+        mb.depthFirst(root_node.ID, nodeList, dest_node);
         
     }
 }
